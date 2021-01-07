1d94155631c73c2ba311decba6a3eefa0e81760d
Kean <justkeanlol@gmail.com>
2021-01-07 18:42:47 +0000

    Changed client to a command bot

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 import discord
+from discord.ext import commands
+from datetime import datetime
+import requests
+import json
 import os
 from dotenv import load_dotenv
 load_dotenv() # Load variables from the .env file
@@ -9,10 +13,10 @@ command_prefix = os.getenv("COMMAND_PREFIX")
 
 # Bot customization
 bot_color = 16225050
-bot_footer = "Crypto Price Bot | By Kean#9123"
+bot_footer_text = "Crypto Price Bot | By Kean#9123"
 bot_footer_icon = "https://i.imgur.com/UY53jZM.png"
 
-client = discord.Client(command_prefix=command_prefix)
+client = commands.Bot(command_prefix=command_prefix)
 
 @client.event
 async def on_ready():
