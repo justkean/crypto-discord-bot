bf75d4d576ffa9b75f53e269a4fbd35feced5dc2
Kean <justkeanlol@gmail.com>
2021-01-07 17:26:58 +0000

    Added bot customization variables

--- bot.py
+++ bot.py
@@ -3,8 +3,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv() # Load variables from the .env file
 
+# Bot info
 bot_token = os.getenv("BOT_TOKEN")
 command_prefix = os.getenv("COMMAND_PREFIX")
+
+# Bot customization
+bot_color = 16225050
+bot_footer = "Crypto Price Bot | By Kean#9123"
+bot_footer_icon = "https://i.imgur.com/UY53jZM.png"
 
 client = discord.Client(command_prefix=command_prefix)
 
