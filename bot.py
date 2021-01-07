ac27c6df7d276408a85715a18543078342a84a67
Kean <justkeanlol@gmail.com>
2021-01-07 18:43:07 +0000

    Added additional customization for currencies

--- bot.py
+++ bot.py
@@ -16,6 +16,13 @@ bot_color = 16225050
 bot_footer_text = "Crypto Price Bot | By Kean#9123"
 bot_footer_icon = "https://i.imgur.com/UY53jZM.png"
 
+# API base link - Documentation: https://www.coingecko.com/en/api
+api_base = "https://api.coingecko.com/api/v3/simple/price?ids="
+
+# Currency information
+currency_name = "USD"
+currency_symbol = "$"
+
 client = commands.Bot(command_prefix=command_prefix)
 
 @client.event
