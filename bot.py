4e95616408e7bf14dc519f39473f015c2d6b167e
Kean <justkeanlol@gmail.com>
2021-01-07 18:43:29 +0000

    Added an error embed system

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ command_prefix = os.getenv("COMMAND_PREFIX")
 
 # Bot customization
 bot_color = 16225050
+bot_error_color = 15549239
 bot_footer_text = "Crypto Price Bot | By Kean#9123"
 bot_footer_icon = "https://i.imgur.com/UY53jZM.png"
 
@@ -24,6 +25,13 @@ currency_name = "USD"
 currency_symbol = "$"
 
 client = commands.Bot(command_prefix=command_prefix)
+
+# To be used whenever there is an error within the bot
+async def send_error_embed(ctx, message):
+    embed = discord.Embed(title="Error", description="An error occured whilst trying to process your last action. You can find more information about the error below.", timestamp=datetime.utcnow(), color=bot_error_color)
+    embed.add_field(name="Message", value=f"```{message}```")
+    embed.set_footer(text=bot_footer_text, icon_url=bot_footer_icon)
+    await ctx.channel.send(embed=embed)
 
 @client.event
 async def on_ready():
